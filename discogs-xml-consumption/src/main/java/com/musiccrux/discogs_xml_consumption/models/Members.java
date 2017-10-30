1f8da21a9d069d9cc0f7cd0de6bccba89934e610
Taiwo Oyekanmi <tooyeka@HeavySeas.wtc.md>
2017-10-30 13:18:22 -0400

    updated members model

--- discogs-xml-consumption/src/main/java/com/musiccrux/discogs_xml_consumption/models/Members.java
+++ discogs-xml-consumption/src/main/java/com/musiccrux/discogs_xml_consumption/models/Members.java
@@ -2,6 +2,7 @@ package com.musiccrux.discogs_xml_consumption.models;
 import java.util.Arrays;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * Represents a person that is member of an {@link Artist}.
@@ -10,21 +11,23 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 
 public class Members {
+	
+	@JsonProperty("id")
+	private long[] discogs_ids;
+	@JsonProperty("name")
+	private String[] names;
 
-	private long[] id;
-	private String[] name;
-
-	public long[] getId() {
-		return id;
+	public long[] getDiscogs_Ids() {
+		return discogs_ids;
 	}
 
-	public String[] getName() {
-		return name;
+	public String[] getNames() {
+		return names;
 	}
 
 	@Override
 	public String toString() {
-		return "Members [id=" + Arrays.toString(id) + ", name=" + Arrays.toString(name) + "]";
+		return "Members [discogs_ids=" + Arrays.toString(discogs_ids) + ", names=" + Arrays.toString(names) + "]";
 	}
 
 }
